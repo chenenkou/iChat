f1fb4028dfd8baf172de47b98dc078b76f393831
chenenkou <chenenkou@163.com>
2023-08-15 17:38:42 +0800

    调整server中的api代码

--- server/app.js
+++ server/app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiChatsRouter = require('./routes/api/chats');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -22,11 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/chats', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-}, apiChatsRouter);
+app.use('/api', cors(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
