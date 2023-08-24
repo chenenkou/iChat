52f921a855f365e9a0f8c1304596921e03fb997d
chenenkou <chenenkou@163.com>
2023-08-24 18:37:33 +0800

    删除提示词组件,配置多语言

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import naive from 'naive-ui'
 import App from './App.vue'
 import router from './router'
+import { setupI18n } from './locales'
 
 const app = createApp(App)
 
 app.use(naive)
 app.use(router)
+setupI18n(app)
 
 const meta = document.createElement('meta')
 meta.name = 'naive-ui-style'
